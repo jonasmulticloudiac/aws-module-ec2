9fab172903205858703304cf8b35aa536a24fb3d
Jonas Isaias <jonas.isaias.ter@estacio.br>
2022-06-08 22:34:07 -0300

    files-aws

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region  = "us-east-1"
+  version = "~> 2.0"
+}
+
+module "ec2" {
+  source                  = "git@github.com:gomex/descomplicando-terraform-modulo-mirror.git?ref=v0.2"
+  app_name                = each.value.app_name
+  instance_type           = each.value.instance_type
+  for_each                = var.projeto
+}
+
+output "ip_address_ec2" {
+  value = values(module.ec2)[*].ip_address
+}
